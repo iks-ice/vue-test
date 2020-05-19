e8adf873f8bcb29a16392397efc522b3c03758c7
Oleh Parshyn <olehparshin87@gmail.com>
2020-05-19 13:58:34 +0300

    mask input func

---
+++ inputMask.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('input');
+
+const cacheInput = () => {
+    let cachedValue = '';
+    return ({target: {value}}) => {
+        const isItNaN = isNaN(value) && value !== '-';
+        const isDecimals = value.indexOf('.') !== -1 && value.slice(value.indexOf('.') + 1).length > 2;
+        if(isItNaN || isDecimals) {
+            return input.value = cachedValue;
+        }
+        cachedValue = value;
+    }
+}
+const maskInput = cacheInput();
+
+input.addEventListener('input', maskInput, false);
